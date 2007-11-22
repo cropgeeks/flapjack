9794fe2f4cb845018ec646d3f0ec4e978c6f8e6b
Iain Milne <imilne@users.noreply.github.com>
2007-11-22 08:54:13 +0000

    Experimenting with dual rendering the display canvas, either:  - in real-time: canvas=large (buffersize=large), boxsize=large, rendertime=fast  - back buffered: canvas=small (buffersize=small), boxsize=small, rendertime=fast Determining the cutoff point between the two methods will probably have to be based on a set [n]MB buffersize limit.

--- j.bat
+++ j.bat
@@ -1,3 +1,8 @@
 @echo off
 
-java -Xmx512m -cp .;classes flapjack.gui.Flapjack %1
+set lib=lib
+
+set fjcp=%lib%\
+set fjcp=%fjcp%;%lib%\sbrn-commons.jar
+
+java -Xmx256m -cp .;classes;%fjcp% flapjack.gui.Flapjack %1
