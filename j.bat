4965aa8827a36f75acc66d909ee439c3759641af
Iain Milne <imilne@users.noreply.github.com>
2008-02-06 10:09:15 +0000

    Added the mysql connector jar and made the necassary modifications to the build and run scripts for using it.

--- j.bat
+++ j.bat
@@ -5,7 +5,9 @@ set lib=lib
 set fjcp=%lib%\
 set fjcp=%fjcp%;%lib%\castor-1.1.2.1-xml.jar
 set fjcp=%fjcp%;%lib%\commons-logging-1.1.1.jar
+set fjcp=%fjcp%;%lib%\mysql-connector-java-5.1.5-bin.jar
 set fjcp=%fjcp%;%lib%\office-2.0.jar
 set fjcp=%fjcp%;%lib%\scri-commons.jar
+set fjcp=%fjcp%;%lib%\swing-layout-1.0.3.jar
 
 java -Xmx768m -cp .;classes;%fjcp% flapjack.gui.Flapjack %1 %2 %3 %4 %5
