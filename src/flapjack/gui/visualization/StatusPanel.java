3d62bd92c9e7b3664474dc0e771d1c385e898cc8
Iain Milne <imilne@users.noreply.github.com>
2008-02-21 15:25:59 +0000

    The status panel is now built by hand rather than Netbeans. Updated the status panel to also show the genotype under the mouse cursor.

--- src/flapjack/gui/visualization/StatusPanel.java
+++ src/flapjack/gui/visualization/StatusPanel.java
@@ -1,5 +1,6 @@
 package flapjack.gui.visualization;
 
+import java.awt.*;
 import java.text.*;
 import javax.swing.*;
 
@@ -13,21 +14,86 @@ class StatusPanel extends JPanel
 	private GenotypePanel gPanel;
 	private GTView view;
 
+	private JLabel label1, label2, label3;
+	private JLabel lineLabel, markerLabel, alleleLabel;
+    private JSlider sizeSlider;
+
 	StatusPanel(GenotypePanel gPanel)
 	{
 		this.gPanel = gPanel;
 
-		initComponents();
+		createControls();
+		layoutControls();
+	}
 
-		label1.setText(RB.getString("gui.visualization.StatusPanel.line"));
-		label2.setText(RB.getString("gui.visualization.StatusPanel.marker"));
-		lineLabel.setText("");
-		markerLabel.setText("");
+/*	void computeDimensions(int w1)
+	{
+		setBorder(BorderFactory.createEmptyBorder(5, w1, 5, 0));
+	}
+*/
 
-		sizeSlider.setMinimum(1);
-		sizeSlider.setMaximum(25);
-		sizeSlider.setValue(7);
+	private void createControls()
+	{
+		label1 = new JLabel(RB.getString("gui.visualization.StatusPanel.line"));
+		label2 = new JLabel(RB.getString("gui.visualization.StatusPanel.marker"));
+		label3 = new JLabel("Genotype:");
+
+		lineLabel = new JLabel();
+		lineLabel.setForeground(Color.red);
+		markerLabel = new JLabel();
+		markerLabel.setForeground(Color.red);
+		alleleLabel = new JLabel();
+		alleleLabel.setForeground(Color.red);
+
+		sizeSlider = new JSlider(1, 25, 7);
 		sizeSlider.addChangeListener(gPanel);
+	}
+
+	private void layoutControls()
+	{
+		// Left hand side labels
+		JPanel p1 = new JPanel(new GridLayout(2, 1, 2, 2));
+		p1.add(label1);
+		p1.add(label2);
+
+		JPanel p2 = new JPanel(new GridLayout(2, 1, 2, 2));
+		p2.add(lineLabel);
+		p2.add(markerLabel);
+
+		JPanel f1 = new JPanel(new BorderLayout(10, 0));
+		f1.add(p1, BorderLayout.WEST);
+		f1.add(p2);
+		f1.setPreferredSize(new Dimension(200, f1.getPreferredSize().height));
+
+
+		// Right hand side labels
+		JPanel p3 = new JPanel(new GridLayout(2, 1, 2, 2));
+		p3.add(label3);
+
+		JPanel p4 = new JPanel(new GridLayout(2, 1, 2, 2));
+		p4.add(alleleLabel);
+
+		JPanel f2 = new JPanel(new BorderLayout(10, 0));
+		f2.add(p3, BorderLayout.WEST);
+		f2.add(p4);
+
+
+		// Panel to hold the left/right hand sides
+		JPanel g1 = new JPanel(new FlowLayout());
+		g1.setBorder(BorderFactory.createEmptyBorder(5, 0, 5, 5));
+		g1.add(f1);
+		g1.add(f2);
+
+
+		// Panel for the slider
+		JPanel sliderPanel = new JPanel(new FlowLayout());
+		sliderPanel.add(sizeSlider);
+
+
+		// Final layout
+		setLayout(new BorderLayout());
+		add(g1, BorderLayout.WEST);
+		add(sliderPanel, BorderLayout.EAST);
 	}
 
 	JSlider getSlider()
@@ -38,91 +104,38 @@ class StatusPanel extends JPanel
 		this.view = view;
 	}
 
-	void setLineIndex(int lineIndex)
+	void setIndices(int lineIndex, int markerIndex)
 	{
+		// Current line under the mouse
 		if (lineIndex < 0 || lineIndex >= view.getLineCount())
 			lineLabel.setText("");
-
 		else
 		{
 			String position = (lineIndex+1) + "/" + view.getLineCount();
 			lineLabel.setText(view.getLine(lineIndex).getName() + " (" + position + ")");
 		}
-	}
 
-	void setMarkerIndex(int markerIndex)
-	{
+		// Current marker under the mouse
 		if (markerIndex < 0 || markerIndex >= view.getMarkerCount())
 			markerLabel.setText("");
-
 		else
 		{
 			Marker m = view.getMarker(markerIndex);
 			markerLabel.setText(m.getName() + " (" + d.format(m.getPosition()) + ")");
 		}
+
+		// Current allele under the mouse
+		if (lineIndex < 0 || lineIndex >= view.getLineCount() ||
+			markerIndex < 0 || markerIndex >= view.getMarkerCount())
+		{
+			alleleLabel.setText("");
+		}
+		else
+		{
+			int stateCode = view.getState(lineIndex, markerIndex);
+			AlleleState state = view.getStateTable().getAlleleState(stateCode);
+
+			alleleLabel.setText(state.getRawData());
+		}
 	}
-
-    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
-    private void initComponents()
-    {
-
-        sizeSlider = new javax.swing.JSlider();
-        label1 = new javax.swing.JLabel();
-        label2 = new javax.swing.JLabel();
-        markerLabel = new javax.swing.JLabel();
-        lineLabel = new javax.swing.JLabel();
-
-        label1.setText("Line:");
-
-        label2.setText("Marker:");
-
-        markerLabel.setForeground(java.awt.Color.red);
-        markerLabel.setText("textextextextextextextext");
-
-        lineLabel.setForeground(new java.awt.Color(255, 0, 0));
-        lineLabel.setText("textextextextextextextext");
-
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
-        this.setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(label1)
-                    .addComponent(label2))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(markerLabel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(lineLabel))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 47, Short.MAX_VALUE)
-                .addComponent(sizeSlider, javax.swing.GroupLayout.PREFERRED_SIZE, 157, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap())
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(label1)
-                            .addComponent(lineLabel))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(label2)
-                            .addComponent(markerLabel)))
-                    .addComponent(sizeSlider, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-        );
-    }// </editor-fold>//GEN-END:initComponents
-
-
-    // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel label1;
-    private javax.swing.JLabel label2;
-    private javax.swing.JLabel lineLabel;
-    private javax.swing.JLabel markerLabel;
-    private javax.swing.JSlider sizeSlider;
-    // End of variables declaration//GEN-END:variables
 }
