b2ac977da2b4c93dcb821900630ad9a30241df77
imilne <imilne@5a560554-8283-4d64-b236-d6dbe6cb70d0>
2008-03-07 14:01:26 +0000

    Flapjack now detects and ignores duplicate markers (within chromosomes and across the map). Data importing is now 3x faster because of this.

--- trunk/src/flapjack/io/ChromosomeMapImporter.java
+++ trunk/src/flapjack/io/ChromosomeMapImporter.java
@@ -32,10 +32,21 @@ public class ChromosomeMapImporter
 			// (And its name), using them to create a new marker
 			Marker marker = new Marker(tokens[0], position);
 
-			// Retrieve the map it should be added to
-			ChromosomeMap map = dataSet.getMapByName(tokens[1], true);
-			// And add it
-			map.addMarker(marker);
+			// Check to see if this marker names already exists (in any map)?
+			int mapIndex = dataSet.getMapIndexByMarkerName(marker.getName());
+			if (mapIndex != -1)
+			{
+				System.out.println("Duplicate marker error: " + marker + " ("
+					+ tokens[1] + ") already found in "
+					+ dataSet.getMapByIndex(mapIndex).getName());
+			}
+			else
+			{
+				// Retrieve the map it should be added to
+				ChromosomeMap map = dataSet.getMapByName(tokens[1], true);
+				// And add it
+				map.addMarker(marker);
+			}
 		}
 
 		in.close();
