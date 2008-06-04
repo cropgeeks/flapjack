7af06fcc9c95896aaf9f3b4688d202194e6399f3
imilne <imilne@5a560554-8283-4d64-b236-d6dbe6cb70d0>
2008-06-04 08:43:54 +0000

    Added hyperlink buttons to the sort dialog for 'Select all' and 'Select none' commands to filter the chromosome list.

--- trunk/src/flapjack/gui/dialog/analysis/NBSortLinesPanel.java
+++ trunk/src/flapjack/gui/dialog/analysis/NBSortLinesPanel.java
@@ -1,6 +1,7 @@
 package flapjack.gui.dialog.analysis;
 
 import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
 import javax.swing.event.*;
 import javax.swing.table.*;
@@ -22,7 +23,8 @@ class NBSortLinesPanel extends javax.swing.JPanel
 		linePanel.setBorder(BorderFactory.createTitledBorder(RB.getString("gui.dialog.analysis.NBSortLinesPanel.linePanel.title")));
 		tablePanel.setBorder(BorderFactory.createTitledBorder(RB.getString("gui.dialog.analysis.NBSortLinesPanel.tablePanel.title")));
 		RB.setText(tableLabel, "gui.dialog.analysis.NBSortLinesPanel.tableLabel");
-
+		RB.setText(selectAllLabel, "gui.dialog.analysis.NBSortLinesPanel.selectAllLabel");
+		RB.setText(selectNoneLabel, "gui.dialog.analysis.NBSortLinesPanel.selectNoneLabel");
 
 		view = viewSet.getSelectedView();
 
@@ -35,7 +37,26 @@ class NBSortLinesPanel extends javax.swing.JPanel
 		if (view.mouseOverLine != -1)
 			selectedLine.setSelectedIndex(view.mouseOverLine);
 
+		createLinkLabels();
 		createTable(viewSet);
+	}
+
+	private void createLinkLabels()
+	{
+		selectAllLabel.setCursor(new Cursor(Cursor.HAND_CURSOR));
+		selectNoneLabel.setCursor(new Cursor(Cursor.HAND_CURSOR));
+
+		selectAllLabel.addMouseListener(new MouseAdapter() {
+			public void mouseClicked(MouseEvent event) {
+				selectAll();
+			}
+		});
+
+		selectNoneLabel.addMouseListener(new MouseAdapter() {
+			public void mouseClicked(MouseEvent event) {
+				selectNone();
+			}
+		});
 	}
 
 	private void createTable(GTViewSet viewSet)
@@ -86,6 +107,18 @@ class NBSortLinesPanel extends javax.swing.JPanel
 		return true;
 	}
 
+	private void selectAll()
+	{
+		for (int i = 0; i < table.getRowCount(); i++)
+			table.setValueAt(true, i, 0);
+	}
+
+	private void selectNone()
+	{
+		for (int i = 0; i < table.getRowCount(); i++)
+			table.setValueAt(false, i, 0);
+	}
+
 	// Generates a boolean array with a true/false selected state for each of
 	// the possible chromosomes that could be used in the sort
 	boolean[] getSelectedChromosomes()
@@ -133,6 +166,9 @@ class NBSortLinesPanel extends javax.swing.JPanel
         jScrollPane1 = new javax.swing.JScrollPane();
         table = new javax.swing.JTable();
         tableLabel = new javax.swing.JLabel();
+        selectAllLabel = new javax.swing.JLabel();
+        label2 = new javax.swing.JLabel();
+        selectNoneLabel = new javax.swing.JLabel();
         linePanel = new javax.swing.JPanel();
         selectedLine = new javax.swing.JComboBox();
         lineLabel = new javax.swing.JLabel();
@@ -154,15 +190,29 @@ class NBSortLinesPanel extends javax.swing.JPanel
         tableLabel.setLabelFor(table);
         tableLabel.setText("Only markers from the following selected chromosomes will be used:");
 
+        selectAllLabel.setForeground(new java.awt.Color(0, 0, 255));
+        selectAllLabel.setText("Select all");
+
+        label2.setText("|");
+
+        selectNoneLabel.setForeground(java.awt.Color.blue);
+        selectNoneLabel.setText("Select none");
+
         org.jdesktop.layout.GroupLayout tablePanelLayout = new org.jdesktop.layout.GroupLayout(tablePanel);
         tablePanel.setLayout(tablePanelLayout);
         tablePanelLayout.setHorizontalGroup(
             tablePanelLayout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
-            .add(org.jdesktop.layout.GroupLayout.TRAILING, tablePanelLayout.createSequentialGroup()
+            .add(tablePanelLayout.createSequentialGroup()
                 .addContainerGap()
-                .add(tablePanelLayout.createParallelGroup(org.jdesktop.layout.GroupLayout.TRAILING)
-                    .add(org.jdesktop.layout.GroupLayout.LEADING, jScrollPane1, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 325, Short.MAX_VALUE)
-                    .add(org.jdesktop.layout.GroupLayout.LEADING, tableLabel, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 325, Short.MAX_VALUE))
+                .add(tablePanelLayout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
+                    .add(jScrollPane1, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 325, Short.MAX_VALUE)
+                    .add(tableLabel, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 325, Short.MAX_VALUE)
+                    .add(tablePanelLayout.createSequentialGroup()
+                        .add(selectAllLabel)
+                        .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
+                        .add(label2)
+                        .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
+                        .add(selectNoneLabel)))
                 .addContainerGap())
         );
         tablePanelLayout.setVerticalGroup(
@@ -171,8 +221,13 @@ class NBSortLinesPanel extends javax.swing.JPanel
                 .addContainerGap()
                 .add(tableLabel)
                 .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
-                .add(jScrollPane1, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 161, Short.MAX_VALUE)
-                .addContainerGap())
+                .add(jScrollPane1, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, 153, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
+                .add(tablePanelLayout.createParallelGroup(org.jdesktop.layout.GroupLayout.BASELINE)
+                    .add(selectAllLabel)
+                    .add(label2)
+                    .add(selectNoneLabel))
+                .addContainerGap(org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         linePanel.setBorder(javax.swing.BorderFactory.createTitledBorder("Comparison line:"));
@@ -228,8 +283,11 @@ class NBSortLinesPanel extends javax.swing.JPanel
     private javax.swing.ButtonGroup group1;
     private javax.swing.ButtonGroup group2;
     private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JLabel label2;
     private javax.swing.JLabel lineLabel;
     private javax.swing.JPanel linePanel;
+    private javax.swing.JLabel selectAllLabel;
+    private javax.swing.JLabel selectNoneLabel;
     private javax.swing.JComboBox selectedLine;
     private javax.swing.JTable table;
     private javax.swing.JLabel tableLabel;
