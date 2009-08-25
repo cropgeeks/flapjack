b8b6b294b14ff6e9fb5bc6b033c0397b0232c379
Gordon Stephen <12711372+gordonstephen@users.noreply.github.com>
2009-08-25 14:26:04 +0000

    - NEW: CreateProject can now incorporate QTL data into a project. QTLTrackOptimiser created to deal with optimisation of the number of tracks QTLs need for correct display.

--- src/flapjack/gui/traits/QTLPanel.java
+++ src/flapjack/gui/traits/QTLPanel.java
@@ -3,6 +3,7 @@
 
 package flapjack.gui.traits;
 
+import flapjack.analysis.QTLTrackOptimiser;
 import java.awt.*;
 import java.awt.event.*;
 import java.awt.image.*;
@@ -25,6 +26,8 @@ public class QTLPanel extends JPanel implements ActionListener, ChangeListener
 	private QTLTableModel model;
 
 	private NBQTLControlPanel controls;
+
+	private QTLTrackOptimiser optimiser;
 
 	public QTLPanel(DataSet dataSet)
 	{
@@ -50,13 +53,15 @@ public class QTLPanel extends JPanel implements ActionListener, ChangeListener
 		add(new JScrollPane(table));
 		add(controls, BorderLayout.SOUTH);
 
+		optimiser = new QTLTrackOptimiser(dataSet);
+
 		updateModel(false);
 	}
 
 	public void updateModel(boolean optimize)
 	{
 		if (optimize)
-			optimizeTrackUsage();
+			optimiser.optimizeTrackUsage();
 
 		model = new QTLTableModel(dataSet, table);
 
@@ -127,104 +132,104 @@ public class QTLPanel extends JPanel implements ActionListener, ChangeListener
 		// Redistribute the features across the *new* number of tracks - for
 		// every chromosome in the data
 		for (ChromosomeMap c: dataSet.getChromosomeMaps())
-			setTracks(size, c);
+			optimiser.setTracks(size, c);
 	}
 
-	// Attempt to work out the optimum number of active tracks for a new set
-	public void optimizeTrackUsage()
-	{
-		long s = System.currentTimeMillis();
-
-		// Optimum number of tracks to use
-		int optimum = 0;
-
-		for (int i = 1; i <= 15; i++)
-		{
-			int trackCount = 0;
-
-			// Scan each chromosome
-			for (ChromosomeMap c: dataSet.getChromosomeMaps())
-				if (setTracks(i, c))
-					trackCount = i;
-
-			// And update the best number if required
-			if (trackCount > optimum)
-				optimum = trackCount;
-			// If a higher number wasn't found, then we've reached the optimum
-			else
-				break;
-		}
-
-		// Once we know the best number, reset to that number
-		for (ChromosomeMap c: dataSet.getChromosomeMaps())
-			setTracks(optimum, c);
-
-		System.out.println("Tracks optimised in " + (System.currentTimeMillis()-s + "ms"));
-	}
-
-	private boolean setTracks(int size, ChromosomeMap c)
-	{
-		Vector<Vector<Feature>> trackSet = c.getTrackSet();
-
-		// 1: Move all features onto a temp track
-		Vector<Feature> tmpTrack = new Vector<Feature>();
-		for (Vector<Feature> track: trackSet)
-		{
-			Enumeration<Feature> features = track.elements();
-			while (features.hasMoreElements())
-				tmpTrack.add(features.nextElement());
-		}
-
-		// 2: Sort the features back into order
-		Collections.sort(tmpTrack);
-
-		// 3: Set up the correct number of new tracks
-		trackSet.removeAllElements();
-		for (int i = 0; i < size; i++)
-			trackSet.add(new Vector<Feature>());
-
-		// 4: Distribute the features across the tracks
-		for (Feature f: tmpTrack)
-		{
-			boolean added = false;
-			for (Vector<Feature> track: trackSet)
-			{
-				added = addToTrack(track, f);
-				if (added)
-					break;
-			}
-
-			if (added == false)
-				trackSet.get(0).add(f);
-		}
-
-		// Return true if any QTLs are on the final track
-		int count = trackSet.size();
-		if (count > 0)
-			return trackSet.get(count-1).size() > 0;
-		else
-			return false;
-	}
-
-	// Checks to see if a feature can be added to the end of this track without
-	// clashing with an existing element
-	private boolean addToTrack(Vector<Feature> track, Feature f)
-	{
-		if (track.size() == 0)
-		{
-			track.add(f);
-			return true;
-		}
-
-		Feature prev = track.get(track.size()-1);
-		if (f.getMin() > prev.getMax())
-		{
-			track.add(f);
-			return true;
-		}
-
-		return false;
-	}
+//	// Attempt to work out the optimum number of active tracks for a new set
+//	public void optimizeTrackUsage()
+//	{
+//		long s = System.currentTimeMillis();
+//
+//		// Optimum number of tracks to use
+//		int optimum = 0;
+//
+//		for (int i = 1; i <= 15; i++)
+//		{
+//			int trackCount = 0;
+//
+//			// Scan each chromosome
+//			for (ChromosomeMap c: dataSet.getChromosomeMaps())
+//				if (setTracks(i, c))
+//					trackCount = i;
+//
+//			// And update the best number if required
+//			if (trackCount > optimum)
+//				optimum = trackCount;
+//			// If a higher number wasn't found, then we've reached the optimum
+//			else
+//				break;
+//		}
+//
+//		// Once we know the best number, reset to that number
+//		for (ChromosomeMap c: dataSet.getChromosomeMaps())
+//			setTracks(optimum, c);
+//
+//		System.out.println("Tracks optimised in " + (System.currentTimeMillis()-s + "ms"));
+//	}
+//
+//	private boolean setTracks(int size, ChromosomeMap c)
+//	{
+//		Vector<Vector<Feature>> trackSet = c.getTrackSet();
+//
+//		// 1: Move all features onto a temp track
+//		Vector<Feature> tmpTrack = new Vector<Feature>();
+//		for (Vector<Feature> track: trackSet)
+//		{
+//			Enumeration<Feature> features = track.elements();
+//			while (features.hasMoreElements())
+//				tmpTrack.add(features.nextElement());
+//		}
+//
+//		// 2: Sort the features back into order
+//		Collections.sort(tmpTrack);
+//
+//		// 3: Set up the correct number of new tracks
+//		trackSet.removeAllElements();
+//		for (int i = 0; i < size; i++)
+//			trackSet.add(new Vector<Feature>());
+//
+//		// 4: Distribute the features across the tracks
+//		for (Feature f: tmpTrack)
+//		{
+//			boolean added = false;
+//			for (Vector<Feature> track: trackSet)
+//			{
+//				added = addToTrack(track, f);
+//				if (added)
+//					break;
+//			}
+//
+//			if (added == false)
+//				trackSet.get(0).add(f);
+//		}
+//
+//		// Return true if any QTLs are on the final track
+//		int count = trackSet.size();
+//		if (count > 0)
+//			return trackSet.get(count-1).size() > 0;
+//		else
+//			return false;
+//	}
+//
+//	// Checks to see if a feature can be added to the end of this track without
+//	// clashing with an existing element
+//	private boolean addToTrack(Vector<Feature> track, Feature f)
+//	{
+//		if (track.size() == 0)
+//		{
+//			track.add(f);
+//			return true;
+//		}
+//
+//		Feature prev = track.get(track.size()-1);
+//		if (f.getMin() > prev.getMax())
+//		{
+//			track.add(f);
+//			return true;
+//		}
+//
+//		return false;
+//	}
 
 	// Renderer for the QTL name column of the table
 	class QTLNameRenderer extends DefaultTableCellRenderer
