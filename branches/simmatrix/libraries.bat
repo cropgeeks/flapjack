f48c521b31ff1d18c4f6dfb7f62a6bebe09a8e0e
imilne <imilne@5a560554-8283-4d64-b236-d6dbe6cb70d0>
2013-06-28 12:12:38 +0000

    - NEW: The project is now held within an SQLite wrapper.

--- branches/simmatrix/libraries.bat
+++ branches/simmatrix/libraries.bat
@@ -8,3 +8,4 @@ SET fjcp=%fjcp%;%lib%\commons-logging-1.1.1.jar
 SET fjcp=%fjcp%;%lib%\i4jruntime.jar
 SET fjcp=%fjcp%;%lib%\office-2.0.jar
 SET fjcp=%fjcp%;%lib%\scri-commons.jar
+SET fjcp=%fjcp%;%lib%\sqlite-jdbc-3.7.2.jar
