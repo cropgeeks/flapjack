d4b91adfc7038f69f0c274f128ac833d2cc71d49
gsteph <gsteph@5a560554-8283-4d64-b236-d6dbe6cb70d0>
2013-08-29 10:45:27 +0000

    - NEW: Added a licence panel to Flapjack.

--- trunk/src/flapjack/gui/dialog/AboutDialog.java
+++ trunk/src/flapjack/gui/dialog/AboutDialog.java
@@ -28,10 +28,12 @@ public class AboutDialog extends JDialog implements ActionListener
 		nbPanel = new AboutPanelNB();
 
 		AvatarPanel avatars = new AvatarPanel();
+		AboutLicencePanelNB licencePanel = new AboutLicencePanelNB();
 
 		JTabbedPane tabs = new JTabbedPane();
 		tabs.add(RB.getString("gui.dialog.AboutDialog.tab1"), nbPanel);
-		tabs.add(RB.format("gui.dialog.AboutDialog.tab2", "\u0026"), avatars);
+		tabs.add(RB.getString("gui.dialog.AboutDialog.tab2"), licencePanel);
+		tabs.add(RB.format("gui.dialog.AboutDialog.tab3", "\u0026"), avatars);
 
 		add(tabs);
 		add(createButtons(), BorderLayout.SOUTH);
