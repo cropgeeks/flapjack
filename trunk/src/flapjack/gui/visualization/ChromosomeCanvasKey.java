103ae9e94b60479546af8a910131a4ab0ef20f74
imilne <imilne@5a560554-8283-4d64-b236-d6dbe6cb70d0>
2014-09-12 10:34:30 +0000

    - NEW: Improved the scrolling speed of the ChromosomesPanel (10 pixels rather than 1). - CHG: Tweaked the positioning of the heatmap density key on the ChromosomesPanel. - BUG: The heatmap's high/low values weren't updating correctly on dataset changes.

--- trunk/src/flapjack/gui/visualization/ChromosomeCanvasKey.java
+++ trunk/src/flapjack/gui/visualization/ChromosomeCanvasKey.java
@@ -5,6 +5,8 @@ import java.awt.event.*;
 import java.awt.image.*;
 import javax.swing.*;
 
+import scri.commons.gui.*;
+
 public class ChromosomeCanvasKey extends JPanel
 {
 	private boolean redraw;
@@ -15,74 +17,100 @@ public class ChromosomeCanvasKey extends JPanel
 	private Dimension dimension = new Dimension();
 
 	private ChromosomeCanvas chromCanvas;
+	private Canvas2D canvas2D;
 
 	ChromosomeCanvasKey(ChromosomeCanvas chromCanvas)
 	{
 		this.chromCanvas = chromCanvas;
+
 		// This panel has to detect changes to its size, and recreate the image
 		addComponentListener(new ComponentAdapter() {
 			public void componentResized(ComponentEvent e) {
-				redraw = true;
-				repaint();
+				redraw();
 			}
 		});
 
-		dimension = new Dimension(getWidth(), 16);
+//		dimension = new Dimension(getWidth(), 32);
+
+		setBorder(BorderFactory.createEmptyBorder(0, 25, 0, 25));
+		setBackground(Color.WHITE);
+
+		setLayout(new BorderLayout(5, 5));
+		add(new JLabel(RB.getString("gui.visualization.ChromosomeCanvasKey.label")), BorderLayout.NORTH);
+		add(canvas2D = new Canvas2D());
+		add(new JLabel(), BorderLayout.SOUTH);
 	}
 
-	public void paintComponent(Graphics graphics)
+	public void redraw()
 	{
-		super.paintComponent(graphics);
+		redraw = true;
+		canvas2D.repaint();
+	}
 
-		Graphics2D g = (Graphics2D) graphics;
-
-		g.setColor(Color.WHITE);
-		g.fillRect(0, 0, getWidth(), getHeight());
-
-		if (redraw)
+	private class Canvas2D extends JPanel
+	{
+		Canvas2D()
 		{
-			// What size of viewport buffer do we need?
-			int w = getWidth();
-			int h = getHeight();
+			setBorder(BorderFactory.createLineBorder(Color.GRAY));
+		}
 
-			// Only make a new buffer if we really really need to, as this has
-			// a noticeable effect on performance
-			if (imageViewPort == null ||
-				imageViewPort.getWidth() != w || imageViewPort.getHeight() != h)
+		public void paintComponent(Graphics graphics)
+		{
+			super.paintComponent(graphics);
+
+			Graphics2D g = (Graphics2D) graphics;
+
+			g.setColor(Color.WHITE);
+			g.fillRect(0, 0, getWidth(), getHeight());
+
+			if (redraw)
 			{
-				imageViewPort = (BufferedImage) createImage(w, h);
+				// What size of viewport buffer do we need?
+				int w = getWidth();
+				int h = getHeight();
+
+				// Only make a new buffer if we really really need to, as this has
+				// a noticeable effect on performance
+				if (imageViewPort == null ||
+					imageViewPort.getWidth() != w || imageViewPort.getHeight() != h)
+				{
+					imageViewPort = (BufferedImage) createImage(w, h);
+				}
+
+				Graphics2D gImage = imageViewPort.createGraphics();
+
+				renderCanvas(gImage, w, h);
+				gImage.dispose();
 			}
 
-			Graphics2D gImage = imageViewPort.createGraphics();
-
-			renderCanvas(gImage);
-			gImage.dispose();
+			g.drawImage(imageViewPort, 0, 0, null);
+			redraw = false;
 		}
 
-		g.drawImage(imageViewPort, 0, 0, null);
-		redraw = false;
-	}
-
-	private void renderCanvas(Graphics2D g)
-	{
-		g.setColor(Color.WHITE);
-		g.fillRect(0, 0, getWidth(), getHeight());
-
-		for (int i=0; i < getWidth(); i++)
+		private void renderCanvas(Graphics2D g, int w, int h)
 		{
-			int alpha = 0 + (int) (((255-0) * (255*(i/ (float)getWidth()))) / 255f);
-			g.setColor(new Color(70, 116, 162, alpha));
-			g.drawLine(i, 0, i, 16);
+			g.setColor(Color.WHITE);
+			g.fillRect(0, 0, w, h);
+
+			for (int i=0; i < w; i++)
+			{
+				int alpha = 0 + (int) (((255-0) * (255*(i/ (float)w))) / 255f);
+				g.setColor(new Color(70, 116, 162, alpha));
+				g.drawLine(i, 0, i, h);
+			}
+
+			g.setColor(Color.BLACK);
+			String low = RB.format("gui.visualization.ChromosomeCanvasKey.low",
+				chromCanvas.minMarkerCount());
+			g.drawString(low, 5, 12);
+
+			g.setColor(Color.WHITE);
+			String high = RB.format("gui.visualization.ChromosomeCanvasKey.high",
+				chromCanvas.maxMarkerCount());
+			g.drawString(high, w-5-g.getFontMetrics().stringWidth(high), 12);
 		}
 
-		g.setColor(Color.BLACK);
-		g.drawString("LOW (" + chromCanvas.minMarkerCount() + ")", 5, 12);
-
-		g.setColor(Color.WHITE);
-		String high = "(" + chromCanvas.maxMarkerCount() + ") HIGH";
-		g.drawString(high, getWidth()-5- g.getFontMetrics().stringWidth(high), 12);
+		public Dimension getPreferredSize()
+			{ return new Dimension(0, 16); }
 	}
-
-	public Dimension getPreferredSize()
-		{ return dimension; }
 }
