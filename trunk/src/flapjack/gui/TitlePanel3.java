94870302b8eebf8803b3c462efe861093979a6c7
imilne <imilne@5a560554-8283-4d64-b236-d6dbe6cb70d0>
2015-03-27 13:39:24 +0000

    - NEW: Added a UIScaler class to help with DPI calculations. - CHG: The title panels on the intro panel are now DPI aware. - CHG: Modified the install4j launcher so that it specifies Flapjack as DPI aware on Windows.

--- trunk/src/flapjack/gui/TitlePanel3.java
+++ trunk/src/flapjack/gui/TitlePanel3.java
@@ -13,19 +13,20 @@ public class TitlePanel3 extends JPanel
 	private static final Color lineColor = new Color(207, 219, 234);
 	private static final Color textColor = new Color(75, 105, 150);
 
-	private static final int h = 30;
-
+	private Font font;
 	private String title;
 
 	public TitlePanel3(String title)
 	{
 		this.title = title;
 		setOpaque(false);
+
+		font = UIScaler.getFont(new Font("Dialog", Font.BOLD, 13));
 	}
 
 	public Dimension getPreferredSize()
 	{
-		return new Dimension(50, h);
+		return new Dimension(50, UIScaler.scale(30));
 	}
 
 	public void paintComponent(Graphics graphics)
@@ -37,12 +38,12 @@ public class TitlePanel3 extends JPanel
 		int w = getWidth();
 
 		g.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
-		g.setFont(new Font("Dialog", Font.BOLD, 13));
+		g.setFont(font);
 		g.setColor(textColor);
-		g.drawString(title, 10, 18);
+		g.drawString(title, 10, UIScaler.scale(18));
 
-		g.setPaint(new GradientPaint(0, h, lineColor, w, h, Color.white));
-		g.setStroke(new BasicStroke(3));
-		g.drawLine(10, 26, w-10, 26);
+		g.setPaint(new GradientPaint(0, 0, lineColor, w, 0, Color.white));
+		g.setStroke(new BasicStroke(UIScaler.scale(3)));
+		g.drawLine(10, UIScaler.scale(26), w-10, UIScaler.scale(26));
 	}
 }
