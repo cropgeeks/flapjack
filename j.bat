52a83c120be0b0e7f8d7a0195dd9026aafd26176
Iain Milne <imilne@users.noreply.github.com>
2015-05-06 13:45:51 +0000

    - NEW: Tweaked the Windows run script to be much simpler and also no longer require libraries.bat.

--- j.bat
+++ j.bat
@@ -1,4 +1,5 @@
 @ECHO OFF
-CALL libraries.bat
 
-java -Xmx4096m -cp .;config;res;classes;%fjcp% flapjack.gui.Flapjack %1 %2 %3 %4 %5
+del lib\flapjack.jar
+
+java -Xmx4096m -cp .;config;res;classes;lib\* flapjack.gui.Flapjack %1 %2 %3 %4 %5
