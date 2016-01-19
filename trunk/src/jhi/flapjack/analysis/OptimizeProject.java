eaf09944d0bc7d0359311d585640ced38938663e
imilne <imilne@5a560554-8283-4d64-b236-d6dbe6cb70d0>
2016-01-19 11:46:58 +0000

    - NEW: Added a File->Optimize menu option that performs an sqlite vacuum on the project. - CHG: Tidied up a few of the menu options to make the use of ellipses a bit more consistent. - CHG: Updated scri-commons to the latest version (to allow for progress tracking dialogs that can't be cancelled).

---
+++ trunk/src/jhi/flapjack/analysis/OptimizeProject.java
@@ -0,0 +1,32 @@
+// Copyright 2009-2015 Information & Computational Sciences, JHI. All rights
+// reserved. Use is subject to the accompanying licence terms.
+
+package jhi.flapjack.analysis;
+
+import jhi.flapjack.io.*;
+
+import scri.commons.gui.*;
+
+public class OptimizeProject extends SimpleJob
+{
+	@Override
+	public int getValue()
+		{ return 0; }
+
+	@Override
+	public void runJob(int jobNum)
+	{
+		try
+		{
+			long s = System.currentTimeMillis();
+			ProjectSerializerDB.vacuum();
+			long e = System.currentTimeMillis();
+
+			System.out.println("Optimize time: " + (e-s) + "ms");
+		}
+		catch (Exception e)
+		{
+			System.out.println(e);
+		}
+	}
+}
