334849fec41174f750bdc0c3cae50ab70d311c15
Iain Milne <imilne@users.noreply.github.com>
2016-05-04 14:31:57 +0000

    - NEW: Ported over the TraitTable's code for doing cell rendering to the LineDataTable, and modified MABC and Traits to work with it.

--- src/jhi/flapjack/gui/mabc/MabcPanel.java
+++ src/jhi/flapjack/gui/mabc/MabcPanel.java
@@ -29,8 +29,6 @@ public class MabcPanel extends JPanel implements ActionListener
 		controls = new MabcPanelNB();
 
 		table = controls.table;
-		table.setDefaultRenderer(Float.class, new TraitCellRenderer());
-		table.setDefaultRenderer(String.class, new TraitCellRenderer());
 
 		setLayout(new BorderLayout());
 		add(new TitlePanel("MABC Results"), BorderLayout.NORTH);
@@ -46,49 +44,10 @@ public class MabcPanel extends JPanel implements ActionListener
 	{
 		model = new MabcTableModel(dataSet, lineStats);
 
-		table.setRowSorter(new TableRowSorter<MabcTableModel>(model));
 		table.setModel(model);
-
-		// Size and set the editor for each column
-		for (int i = 0; i < table.getColumnCount(); i++)
-		{
-			TableColumn column = table.getColumnModel().getColumn(i);
-			column.setPreferredWidth(120);
-		}
-
-		NumberFormat nf = NumberFormat.getInstance();
-//		controls.coverageLabel.setText("RPP Coverage: " + nf.format(lineStats.get(0).getCoverage()));
 	}
 
 	public void actionPerformed(ActionEvent e)
 	{
-	}
-
-	// Deals with the fact that our fake double header for the JTable means
-	// that String data can be found in numerical columns.
-	public class TraitCellRenderer extends DefaultTableCellRenderer
-	{
-		protected final NumberFormat nf = NumberFormat.getInstance();
-
-		private Color bgCol1 = UIManager.getColor("Table.selectionBackground");
-		private Color bgCol2 = UIManager.getColor("Table.background");
-
-		@Override
-		public Component getTableCellRendererComponent(JTable table, Object value,
-			boolean isSelected, boolean hasFocus, int row, int column)
-		{
-			super.getTableCellRendererComponent(table, value, isSelected,
-				hasFocus, row, column);
-
-			if (value instanceof Float && value != null)
-			{
-				setText(nf.format((Number)value));
-				setHorizontalAlignment(JLabel.RIGHT);
-			}
-			else
-				setHorizontalAlignment(JLabel.LEFT);
-
-			return this;
-		}
 	}
 }
