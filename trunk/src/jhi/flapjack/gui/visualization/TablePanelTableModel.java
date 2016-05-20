50ed8b8d44b6e4ec27f2a54122a1df3d9668163f
gsteph <gsteph@5a560554-8283-4d64-b236-d6dbe6cb70d0>
2016-05-20 13:02:33 +0000

    - NEW: Added the ability to display MABC stats in the main view.

--- trunk/src/jhi/flapjack/gui/visualization/TablePanelTableModel.java
+++ trunk/src/jhi/flapjack/gui/visualization/TablePanelTableModel.java
@@ -1,6 +1,7 @@
 package jhi.flapjack.gui.visualization;
 
 import jhi.flapjack.data.*;
+import jhi.flapjack.data.results.MABCLineStats;
 import jhi.flapjack.gui.table.*;
 
 import java.text.DecimalFormat;
@@ -10,18 +11,53 @@ import java.text.DecimalFormat;
  */
 public class TablePanelTableModel extends LineDataTableModel
 {
-	private GTViewSet viewSet;
+	private final GTViewSet viewSet;
+	private final boolean showMabc;
 
-	private DecimalFormat df = new DecimalFormat("0.000");
+	private DecimalFormat df = new DecimalFormat("0.00");
 
-	public TablePanelTableModel(GTViewSet viewSet)
+	private int chrCount;
+	private int qtlCount;
+	private int rppIndex;
+	private int rppTotalIndex;
+	private int rppCoverageIndex;
+	private int qtlIndex;
+
+	public TablePanelTableModel(GTViewSet viewSet, boolean showMabc)
 	{
 		this.viewSet = viewSet;
+		this.showMabc = showMabc;
 
-		if (viewSet == null || !viewSet.getDisplayLineScores())
-			columnNames = new String [] { "" };
-		else
-			columnNames = new String[] { "", "" };
+		int noCols = 1;
+
+		if (viewSet != null)
+		{
+			if (viewSet.getDisplayLineScores())
+				noCols++;
+
+
+			if (showMabc)
+			{
+				MABCLineStats lineStats = viewSet.getLines().get(0).results().getMABCLineStats();
+				if (lineStats != null)
+				{
+					// Use information from the first result to determine the UI
+					chrCount = lineStats.getChrScores().size();
+					qtlCount = lineStats.getQTLScores().size();
+
+					// Column indices
+					rppIndex = noCols;
+					rppTotalIndex = rppIndex + chrCount;
+					rppCoverageIndex = rppTotalIndex + 1;
+					qtlIndex = rppCoverageIndex + 1;
+
+					// TODO: UPDATE!
+					noCols = qtlIndex + (qtlCount * 2);
+				}
+			}
+		}
+
+		columnNames = new String[noCols];
 	}
 
 	@Override
@@ -35,17 +71,46 @@ public class TablePanelTableModel extends LineDataTableModel
 	{
 		if (columnIndex == 0)
 			return viewSet.getLines().get(rowIndex);
-		else
+
+		else if (viewSet.getDisplayLineScores() && columnIndex == 1)
 			return df.format(viewSet.getLines().get(rowIndex).getScore());
+
+		else if (showMabc)
+		{
+			MABCLineStats stats = viewSet.getLines().get(rowIndex).results().getMABCLineStats();
+			if (stats != null)
+			{
+				if (columnIndex >= rppIndex && columnIndex < rppTotalIndex)
+					return df.format(stats.getChrScores().get(columnIndex - rppIndex).sumRP);
+
+				else if (columnIndex == rppTotalIndex)
+					return df.format(stats.getRPPTotal());
+
+				else if (columnIndex == rppCoverageIndex)
+					return df.format(stats.getGenomeCoverage());
+
+				else if (columnIndex >= qtlIndex)
+				{
+					columnIndex = columnIndex - qtlIndex;
+					int qtl = columnIndex / 2;
+
+					MABCLineStats.QTLScore score = stats.getQTLScores().get(qtl);
+
+					if (columnIndex % 2 == 0)
+						return df.format(score.drag);
+					else
+						return score.status ? 1 : 0;
+				}
+			}
+		}
+		return -1;
 	}
 
 	@Override
 	public Class<?> getColumnClass(int columnIndex) {
 		if (columnIndex == 0)
 			return LineInfo.class;
-		else if (columnIndex == 1)
-			return String.class;
 		else
-			return Integer.class;
+			return Double.class;
 	}
 }
