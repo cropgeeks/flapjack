8f10d6ce00ed7b5a9072b7d03d39356306550ab6
Iain Milne <imilne@users.noreply.github.com>
2016-09-14 09:29:02 +0000

    - NEW: Added some missing files for a previous commit.

---
+++ src/jhi/flapjack/data/results/MabcChrScore.java
@@ -0,0 +1,58 @@
+// Copyright 2009-2016 Information & Computational Sciences, JHI. All rights
+// reserved. Use is subject to the accompanying licence terms.
+
+package jhi.flapjack.data.results;
+
+import jhi.flapjack.data.*;
+
+public class MabcChrScore extends XMLRoot
+{
+	public GTView view;
+	public double coverage;
+	public double sumRP;
+	public double sumDO;
+
+	public MabcChrScore()
+	{
+	}
+
+	public GTView getView()
+	{
+		return view;
+	}
+
+	public void setView(GTView view)
+	{
+		this.view = view;
+	}
+
+	public double getCoverage()
+	{
+		return coverage;
+	}
+
+	public void setCoverage(double coverage)
+	{
+		this.coverage = coverage;
+	}
+
+	public double getSumRP()
+	{
+		return sumRP;
+	}
+
+	public void setSumRP(double sumRP)
+	{
+		this.sumRP = sumRP;
+	}
+
+	public double getSumDO()
+	{
+		return sumDO;
+	}
+
+	public void setSumDO(double sumDO)
+	{
+		this.sumDO = sumDO;
+	}
+}
---
+++ src/jhi/flapjack/data/results/MabcQtlScore.java
@@ -0,0 +1,54 @@
+// Copyright 2009-2016 Information & Computational Sciences, JHI. All rights
+// reserved. Use is subject to the accompanying licence terms.
+
+package jhi.flapjack.data.results;
+
+import jhi.flapjack.data.*;
+
+public class MabcQtlScore extends XMLRoot
+{
+	public QTLInfo qtl;
+	public double drag;
+	public int status = 1;
+
+	public MabcQtlScore()
+	{
+	}
+
+	public MabcQtlScore(QTLInfo qtl)
+	{
+		this.qtl = qtl;
+	}
+
+	public QTLInfo getQtl()
+	{
+		return qtl;
+	}
+
+	public void setQtl(QTLInfo qtl)
+	{
+		this.qtl = qtl;
+	}
+
+	public double getDrag()
+	{
+		return drag;
+	}
+
+	public void setDrag(double drag)
+	{
+		this.drag = drag;
+	}
+
+	public int getStatus()
+	{
+		return status;
+	}
+
+	public void setStatus(int status)
+	{
+		this.status = status;
+	}
+
+	
+}
