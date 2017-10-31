4b01bc0ad98c292cd2663ef4dea4f14f0ca9c0a9
gsteph <gsteph@5a560554-8283-4d64-b236-d6dbe6cb70d0>
2017-10-31 11:47:43 +0000

    - NEW: Added specific launcher for bioconda version checks.

---
+++ trunk/src/jhi/flapjack/gui/FlapjackVersion.java
@@ -0,0 +1,13 @@
+// Copyright 2009-2017 Information & Computational Sciences, JHI. All rights
+// reserved. Use is subject to the accompanying licence terms.
+
+package jhi.flapjack.gui;
+
+public class FlapjackVersion
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Flapjack 1.16.10.31");
+		System.exit(0);
+	}
+}
