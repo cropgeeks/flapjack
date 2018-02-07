f53ca392f0a98817542df02646c4b432859b7db9
Iain Milne <imilne@users.noreply.github.com>
2018-02-07 14:10:22 +0000

    - NEW: Hackathon: Reworked the code for collapsing A/T:T/A situations to (hopefully) run much faster on cases with a lot of duplicates.

--- src/jhi/flapjack/analysis/OptimizeStateTable.java
+++ src/jhi/flapjack/analysis/OptimizeStateTable.java
@@ -3,6 +3,8 @@
 
 package jhi.flapjack.analysis;
 
+import java.util.*;
+
 import jhi.flapjack.data.*;
 
 public class OptimizeStateTable
@@ -37,6 +39,20 @@ public class OptimizeStateTable
 
 	public void optimize(boolean compareHomzOnly)
 	{
+		// This lookup table will hold original index (in the state table) value
+		// compared against new index (eg 1=1 if no change, or 4=2 if remapped)
+		HashMap<Integer,Integer> remap = new HashMap<>();
+		for (int i = 0; i < stateTable.size(); i++)
+			remap.put(i,i);
+
+		System.out.println("BEFORE");
+		for (int key: remap.keySet())
+		{
+			System.out.println("Remap: " + key + ": " + remap.get(key) + " - " + stateTable.getAlleleState(remap.get(key)));
+		}
+
+		ArrayList<Integer> toRemove = new ArrayList<>();
+
 		// Start at index 1, because we never attempt to match the unknown state
 		// Compare every allele state with every other allele state...
 		for (int i = 1; i < stateTable.size(); i++)
@@ -54,32 +70,38 @@ public class OptimizeStateTable
 				else if (compareHomzOnly && s1.isSameHomzAs(s2) == false)
 					continue;
 
+				// Overwrite the original (higher,duplicate) value with the
+				// current index (which is the one we want to use instead)
+				remap.put(j, i);
 
-				// Update the array, rewriting all the duplicates
-				System.out.println("Collapsing " + s1 + " and " + s2);
-				collapse(i, j);
+				// Decrement all higher values too
+				for (int k = j+1; k < stateTable.size(); k++)
+					remap.put(k, k-1);
 
-				// But also rewrite every value greater than the duplicate
-				// position, because if we remove an element, all successive
-				// elements need to point to an index one less than before
-				for (int k = j; k < stateTable.size()-1; k++)
-					collapse(k, k+1);
-
-				stateTable.deleteState(j);
-
-				// Drop j back down one value, so the search continues from the
-				// (new) proper position in the state table
-				j--;
+				// Track which element to remove (sticking it at the start of
+				// the list, so higher elements are removed first)
+				toRemove.add(0, j);
 			}
 		}
-	}
 
-	private void collapse(int s1, int s2)
-	{
-		for (int i = 0; i < dataSet.countLines(); i++)
+		// Remove the duplicates from the state table
+		for (int index: toRemove)
+			stateTable.deleteState(index);
+
+		System.out.println("AFTER");
+		for (int key: remap.keySet())
 		{
-			Line line = dataSet.getLineByIndex(i);
-			line.collapseStates(s1, s2);
+			System.out.println("Remap: " + key + ": " + remap.get(key));
+		}
+
+		// And finally update the data arrays to these new values
+		if (toRemove.size() > 0)
+		{
+			for (int i = 0; i < dataSet.countLines(); i++)
+			{
+				Line line = dataSet.getLineByIndex(i);
+				line.collapseStates(remap);
+			}
 		}
 	}
 }
