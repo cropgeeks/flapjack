18a4f250e06baf8d044a69ac9bb9cd00f2174a3b
Gordon Stephen <12711372+gordonstephen@users.noreply.github.com>
2018-04-17 12:03:53 +0000

    - BUG: Missing classes from previous commits (1345-1346).

---
+++ src/jhi/flapjack/analysis/FilterHeterozygousLines.java
@@ -0,0 +1,66 @@
+package jhi.flapjack.analysis;
+
+import jhi.flapjack.data.*;
+
+import scri.commons.gui.*;
+
+// Analysis to filter out lines which have a percentage of heterozygous data greater than a given cutoff value
+public class FilterHeterozygousLines extends SimpleJob
+{
+	private final GTViewSet viewSet;
+	private final StateTable stateTable;
+	private final boolean[] selectedChromosomes;
+	private final int cutoff;
+	private int count;
+
+	public FilterHeterozygousLines(GTViewSet viewSet, boolean[] selectedChromosomes, int cutoff)
+	{
+		this.viewSet = viewSet;
+		this.stateTable = this.viewSet.getDataSet().getStateTable();
+		this.selectedChromosomes = selectedChromosomes;
+		this.cutoff = cutoff;
+	}
+
+	public void runJob(int index)
+		throws Exception
+	{
+		// Analysis runs over the selected lines and markers (we do this as standard)
+		AnalysisSet as = new AnalysisSet(viewSet)
+			.withViews(selectedChromosomes)
+			.withSelectedLines()
+			.withSelectedMarkers();
+
+		// The total in this case is the number of lines across all views
+		for (int i = 0; i < as.viewCount(); i++)
+			maximum += as.lineCount();
+
+		for (int line=as.lineCount()-1; line >= 0; line--)
+		{
+			int allelesCount = 0;
+			int heterozygousCount = 0;
+
+			for (int view=0; view < as.viewCount(); view++)
+			{
+				for (int marker=0; marker < as.markerCount(view); marker++)
+				{
+					if (stateTable.isHet(as.getState(view, line, marker)))
+						heterozygousCount++;
+
+					allelesCount++;
+				}
+			}
+
+			if ((heterozygousCount / (float)allelesCount)*100 >= cutoff)
+			{
+				// TODO: What's the best way of getting to hideLine from here?
+				as.getGTView(0).hideLine(line);
+				count++;
+			}
+
+			progress++;
+		}
+	}
+
+	public int getCount()
+		{ return count; }
+}
---
+++ src/jhi/flapjack/analysis/FilterMissingLines.java
@@ -0,0 +1,64 @@
+package jhi.flapjack.analysis;
+
+import jhi.flapjack.data.*;
+
+import scri.commons.gui.*;
+
+// Analysis to filter out lines which have a percentage of missing data greater than a given cutoff value
+public class FilterMissingLines extends SimpleJob
+{
+	private GTViewSet viewSet;
+	private boolean[] selectedChromosomes;
+	private int cutoff;
+	private int count;
+
+	public FilterMissingLines(GTViewSet viewSet, boolean[] selectedChromosomes, int cutoff)
+	{
+		this.viewSet = viewSet;
+		this.selectedChromosomes = selectedChromosomes;
+		this.cutoff = cutoff;
+	}
+
+	public void runJob(int index)
+		throws Exception
+	{
+		// Analysis runs over the selected lines and markers (we do this as standard)
+		AnalysisSet as = new AnalysisSet(viewSet)
+			.withViews(selectedChromosomes)
+			.withSelectedLines()
+			.withSelectedMarkers();
+
+		// The total in this case is the number of lines multiplied by the number of markers
+		for (int i = 0; i < as.viewCount(); i++)
+			maximum += as.lineCount();
+
+		for (int line=as.lineCount()-1; line >= 0; line--)
+		{
+			int allelesCount = 0;
+			int missingCount = 0;
+
+			for (int view=0; view < as.viewCount(); view++)
+			{
+				for (int marker=0; marker < as.markerCount(view); marker++)
+				{
+					if (as.getState(view, line, marker) == 0)
+						missingCount++;
+
+					allelesCount++;
+				}
+			}
+
+			if ((missingCount / (float)allelesCount)*100 >= cutoff)
+			{
+				// TODO: What's the best way of getting to hideLine from here?
+				as.getGTView(0).hideLine(line);
+				count++;
+			}
+
+			progress++;
+		}
+	}
+
+	public int getCount()
+		{ return count; }
+}
