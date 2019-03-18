a11c34dcd24a5eb8290b91480cda157f78037edf
Gordon Stephen <12711372+gordonstephen@users.noreply.github.com>
2019-03-18 14:14:51 +0000

    - CHG: Disable forward breeding analysis menu so we can make a public release. - CHG: Update the BrapiClient to point at the brapi resources xml for this release.

--- src/jhi/flapjack/io/brapi/BrapiClient.java
+++ src/jhi/flapjack/io/brapi/BrapiClient.java
@@ -528,7 +528,7 @@ public class BrapiClient
 	public XmlBrapiProvider getBrapiProviders()
 		throws Exception
 	{
-		URL url = new URL("https://ics.hutton.ac.uk/resources/brapi/flapjack-brapi-201901-v1.1.zip");
+		URL url = new URL("https://ics.hutton.ac.uk/resources/brapi/flapjack-brapi-201903-v1.1.zip");
 
 		File dir = new File(FlapjackUtils.getCacheDir(), "brapi");
 		dir.mkdirs();
