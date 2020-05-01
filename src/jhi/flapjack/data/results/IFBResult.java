078d5cca961f4c765c0f20ae12b5fe7485422bd6
Iain Milne <imilne@users.noreply.github.com>
2020-05-01 13:47:24 +0100

    - NEW: Added placeholder variables for sum totals (across QTL) in the IFBResult object.

--- src/jhi/flapjack/data/results/IFBResult.java
+++ src/jhi/flapjack/data/results/IFBResult.java
@@ -11,6 +11,9 @@ public class IFBResult extends XMLRoot
 {
 	private ArrayList<IFBQTLScore> qtlScores = new ArrayList<>();
 
+	private double mbvTotal;
+	private double wmbvTotal;
+
 	public IFBResult()
 	{
 	}
@@ -21,4 +24,15 @@ public class IFBResult extends XMLRoot
 	public void setQtlScores(ArrayList<IFBQTLScore> qtlScores)
 		{ this.qtlScores = qtlScores; }
 
+	public double getMbvTotal()
+		{ return mbvTotal; }
+
+	public void setMbvTotal(double mbvTotal)
+		{ this.mbvTotal = mbvTotal; }
+
+	public double getWmbvTotal()
+		{ return wmbvTotal; }
+
+	public void setWmbvTotal(double wmbvTotal)
+		{ this.wmbvTotal = wmbvTotal; }
 }
