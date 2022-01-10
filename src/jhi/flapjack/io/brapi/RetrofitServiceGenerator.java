c1e2fdd193c89aac35b1199a88cc5c2937b20809
Iain Milne <imilne@users.noreply.github.com>
2022-01-10 13:38:18 +0000

    - CHG: Updated the licence headers for 2022.

--- src/jhi/flapjack/io/brapi/RetrofitServiceGenerator.java
+++ src/jhi/flapjack/io/brapi/RetrofitServiceGenerator.java
@@ -1,3 +1,6 @@
+// Copyright 2007-2022 Information & Computational Sciences, JHI. All rights
+// reserved. Use is subject to the accompanying licence terms.
+
 package jhi.flapjack.io.brapi;
 
 import java.net.*;
